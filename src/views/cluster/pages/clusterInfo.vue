419870380269dad7d84d99be45771c5cfd94c92d
see92 <dsq_1105@outlook.com>
2022-12-29 09:40:56 +0800

    cluster---update

--- src/views/cluster/pages/clusterInfo.vue
+++ src/views/cluster/pages/clusterInfo.vue
@@ -1,10 +1,32 @@
 <template>
-  <div>111</div>
+  <b-card class="card-pd">
+    <validation-observer ref="cluForm" #default="{ invalid }">
+      <b-form autocomplete="off">
+        <b-tabs>
+          <b-tab title="常规设置"></b-tab>
+          <b-tab title="服务引擎"></b-tab>
+          <b-tab title="存储"></b-tab>
+          <b-tab title="DB"></b-tab>
+          <b-tab title="Redis"></b-tab>
+          <b-tab title="ES"></b-tab>
+        </b-tabs>
+      </b-form>
+    </validation-observer>
+  </b-card>
 </template>
 
 <script>
+import { BCard, BForm, BTab, BTabs } from "bootstrap-vue";
+import { ValidationObserver } from "vee-validate";
 export default {
   name: "clusterInfo",
+  components: {
+    BCard,
+    BForm,
+    BTab,
+    BTabs,
+    ValidationObserver,
+  },
 };
 </script>
 
